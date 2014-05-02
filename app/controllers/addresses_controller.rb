509c574dd79b0c2340bc3a02546cc5dacffed803
Todd S <starco58@gmail.com>
2014-05-01 20:59:39 -0500

    hide lat/lng code, test using @url_safe_address
    works (no errors)

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,14 +3,14 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Sheridan"
+    @address = "the corner of Foster and Western"
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=@url_safe_address&sensor=false"
     raw_data = open(url).read
-    parsed_data = JSON.parse(raw_data)
-    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    # parsed_data = JSON.parse(raw_data)
+    # @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    # @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
   end
 end
