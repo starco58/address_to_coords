9427834115a06c70e125f6a06741d234c18b921e
Todd S <starco58@gmail.com>
2014-05-01 20:56:45 -0500

    completed step one of lat and lng coord
    need to figure out the @address reference

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -10,7 +10,7 @@ class AddressesController < ApplicationController
     url = "http://maps.googleapis.com/maps/api/geocode/json?address=the+corner+of+Foster+and+Sheridan&sensor=false"
     raw_data = open(url).read
     parsed_data = JSON.parse(raw_data)
-    # @latitude = ?
-    # @longitude = ?
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
   end
 end
