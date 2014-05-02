8b4cf061a622a102e451fce23a53cbe736d6bc23
Todd S <starco58@gmail.com>
2014-05-01 21:02:15 -0500

    I think this one works....
    lat and lng have readings, but not sure if the right lat/lng

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -3,14 +3,14 @@ require 'json'
 
 class AddressesController < ApplicationController
   def fetch_coordinates
-    @address = "the corner of Foster and Western"
+    @address = "the corner of Foster and Sheridan"
     @url_safe_address = URI.encode(@address)
 
     # Your code goes here.
-    url = "http://maps.googleapis.com/maps/api/geocode/json?address=@url_safe_address&sensor=false"
+    url = "http://maps.googleapis.com/maps/api/geocode/json?address=@address&sensor=false"
     raw_data = open(url).read
-    # parsed_data = JSON.parse(raw_data)
-    # @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
-    # @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
+    parsed_data = JSON.parse(raw_data)
+    @latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+    @longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
   end
 end
